dbb1fb22a7735836eecde9523dd7ec8fba0aad5a
Dostan <turlybekdostan@gmail.com>
2025-07-26 11:26:04 +0500

    Update new version of aperance?

--- RecordDiary4/Services/CustomColorService.swift
+++ RecordDiary4/Services/CustomColorService.swift
@@ -9,18 +9,31 @@ import Foundation
 import SwiftUI
 
 class CustomColorService {
-    static let red = Color(#colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1))
-    static let orange = Color(#colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1))
-    static let yellow = Color(#colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1))
-    static let green = Color(#colorLiteral(red: 0.8321695924, green: 0.985483706, blue: 0.4733308554, alpha: 1))
-    static let blue = Color(#colorLiteral(red: 0.4620226622, green: 0.8382837176, blue: 1, alpha: 1))
-    static let purple = Color(#colorLiteral(red: 0.8446564078, green: 0.5145705342, blue: 1, alpha: 1))
-    static let pink = Color(#colorLiteral(red: 0.9932022691, green: 0.3784358501, blue: 0.7091116905, alpha: 1))
-    static let cyan = Color(#colorLiteral(red: 0.4508578777, green: 0.9882974029, blue: 0.8376303315, alpha: 1))
-    static let grayLight = Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1))
-    static let black = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
-    static let white = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
-    static let beige = Color(#colorLiteral(red: 0.9899074435, green: 0.8228895664, blue: 0.6137205958, alpha: 1))
+//    static let red = Color(#colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1))
+//    static let orange = Color(#colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1))
+//    static let yellow = Color(#colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1))
+//    static let green = Color(#colorLiteral(red: 0.8321695924, green: 0.985483706, blue: 0.4733308554, alpha: 1))
+//    static let blue = Color(#colorLiteral(red: 0.4620226622, green: 0.8382837176, blue: 1, alpha: 1))
+//    static let purple = Color(#colorLiteral(red: 0.8446564078, green: 0.5145705342, blue: 1, alpha: 1))
+//    static let pink = Color(#colorLiteral(red: 0.9932022691, green: 0.3784358501, blue: 0.7091116905, alpha: 1))
+//    static let cyan = Color(#colorLiteral(red: 0.4508578777, green: 0.9882974029, blue: 0.8376303315, alpha: 1))
+//    static let grayLight = Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1))
+//    static let black = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+//    static let white = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+//    static let beige = Color(#colorLiteral(red: 0.9899074435, green: 0.8228895664, blue: 0.6137205958, alpha: 1))
+//    
+    static let red = Color("RedAppCustom")
+    static let orange = Color("OrangeAppCustom")
+    static let yellow = Color("YellowAppCustom")
+    static let green = Color("GreenAppCustom")
+    static let blue = Color("BlueAppCustom")
+    static let purple = Color("PurpleAppCustom")
+    static let pink = Color("PinkAppCustom")
+    static let cyan = Color("CyanAppCustom")
+    static let grayLight = Color("GrayLightAppCustom")
+    static let black = Color("BlackAppCustom")
+    static let white = Color("WhiteAppCustom")
+    static let beige = Color("BeigeAppCustom")
 }
 
 extension ColorTheme {
