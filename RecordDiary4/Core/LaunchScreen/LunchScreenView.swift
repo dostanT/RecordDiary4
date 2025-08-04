d6efa4d961e339676402524f191cd72608746915
Dostan <turlybekdostan@gmail.com>
2025-08-04 11:54:25 +0500

    App icon

---
+++ RecordDiary4/Core/LaunchScreen/LunchScreenView.swift
@@ -0,0 +1,17 @@
+//
+//  LunchScreenView.swift
+//  RecordDiary4
+//
+//  Created by Dostan Turlybek on 04.08.2025.
+//
+
+import SwiftUI
+
+struct LunchScreenView: View {
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
---
+++ RecordDiary4/Core/LaunchScreen/LunchScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LunchScreenViewModel.swift
+//  RecordDiary4
+//
+//  Created by Dostan Turlybek on 04.08.2025.
+//
+
+import Foundation
+
+class LaunchScreenViewModel: ObservableObject {
+    
+}
